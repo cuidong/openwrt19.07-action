7775a3ebda6ad7506feef84eba9c02a747dc5074
CUIDONG <cuidong@gmail.com>
2020-12-12 15:24:38 +0800

    Update n3-part2.sh

--- n3-part2.sh
+++ n3-part2.sh
@@ -3,10 +3,10 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
-#Set temperature display
-#sed -i '/<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></td></tr> <tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec("cut -c1-2 /sys/class/thermal/thermal_zone0/temp")%></td></tr>' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
+# Set temperature display
+# sed -i '/<tr><td width="33%"><%:Kernel Version%></td><td><%=unameinfo.release or "?"%></td></tr> <tr><td width="33%"><%:CPU Temperature%></td><td><%=luci.sys.exec("cut -c1-2 /sys/class/thermal/thermal_zone0/temp")%></td></tr>' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 
 #移除无用软件包
 rm -rf luci-theme-argon package/lean
